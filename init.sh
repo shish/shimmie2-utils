ef597f42059303a82938a37c8ff30b4eea4eaffa
Shish <shish@shishnet.org>
2012-03-09 17:22:56 +0000

    tweaks

--- init.sh
+++ init.sh
@@ -39,7 +39,7 @@ sudo chmod g+w logs
 sudo rm -f /etc/nginx/sites-enabled/default
 sed "s#@INSTALLDIR@#`pwd`#" shimmie2-utils/shimtest.nginx.conf | sudo tee /etc/nginx/sites-available/shimtest > /dev/null
 sudo ln -sf /etc/nginx/sites-available/shimtest /etc/nginx/sites-enabled/shimtest
-sudo /etc/init.d/nginx reload
+sudo /etc/init.d/nginx restart
 
 sed "s#@USER@#`whoami`#" shimmie2-utils/shimtest.php-fpm.conf | sudo tee /etc/php5/fpm/pool.d/shimtest.conf > /dev/null
-sudo /etc/init.d/php5-fpm reload
+sudo /etc/init.d/php5-fpm restart
